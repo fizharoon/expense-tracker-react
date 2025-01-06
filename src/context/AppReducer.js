1567944cc926644bc9888562cb15dc80c75b3630
fizharoon <fizharoon@gmail.com>
2025-01-05 17:35:23 -0800

    Working on transaction states

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// how we specify application case changes based on actions/state context
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
