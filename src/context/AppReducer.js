5944d3680451a43804ce684eb852b63e59484cb6
fizharoon <fizharoon@gmail.com>
2025-01-09 18:50:38 -0800

    Finished changes to add and delete transactions

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,17 @@
 // how we specify application case changes based on actions/state context
 export default (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return{
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return{
+                ...state,
+                transactions: [action.payload,...state.transactions]
+            }
+
         default:
             return state
     }
